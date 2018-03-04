fb49edc8c161927d4c5a71d5806530590942c8cf
User <user@Users-Mac-mini-3.local>
2018-03-04 18:51:19 -0500

    finished

--- chat/ViewController.swift
+++ chat/ViewController.swift
@@ -8,11 +8,100 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, UITextFieldDelegate {
+    lazy var inputTextField:UITextField = {
+        let TextField = UITextField()
+        TextField.placeholder = "Enter message..."
+        TextField.translatesAutoresizingMaskIntoConstraints = false
+        TextField.delegate = self
+        return TextField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView?.backgroundColor = UIColor.white
         // Do any additional setup after loading the view, typically from a nib.
+        setupInputComponents()
+        
+        setupKeyboardObservers()
+        
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
+    }
+    
+    func setupKeyboardObservers(){
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    @objc func handleKeyboardWillHide(notification: NSNotification){
+        containerViewBottomAnchor?.constant = 0
+    }
+    
+    @objc func handleKeyboardWillShow(notification: NSNotification){
+        let keyboardFrame = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as AnyObject).cgRectValue
+        //print(keyboardFrame?.height)
+        containerViewBottomAnchor?.constant =  -keyboardFrame!.height
+    }
+    
+    @objc func dismissKeyboard() {
+        inputTextField.resignFirstResponder()
+    }
+    
+    
+    var containerViewBottomAnchor: NSLayoutConstraint?
+    
+    func setupInputComponents(){
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(containerView)
+        
+        containerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        
+        containerViewBottomAnchor = containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        containerViewBottomAnchor?.isActive = true
+        
+        containerView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        let sendButton = UIButton(type: .system)
+        sendButton.setTitle("Send", for: .normal)
+        sendButton.translatesAutoresizingMaskIntoConstraints = false
+        sendButton.addTarget(self, action: #selector(handleSend), for: .touchUpInside)
+        containerView.addSubview(sendButton)
+        
+        sendButton.rightAnchor.constraint(equalTo: containerView.rightAnchor).isActive = true
+        sendButton.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        sendButton.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        sendButton.heightAnchor.constraint(equalTo: containerView.heightAnchor).isActive = true
+        
+        containerView.addSubview(inputTextField)
+        inputTextField.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 8).isActive = true
+        inputTextField.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        inputTextField.rightAnchor.constraint(equalTo: sendButton.leftAnchor).isActive = true
+        inputTextField.heightAnchor.constraint(equalTo: containerView.heightAnchor).isActive = true
+        
+        let separatorLineView = UIView()
+        separatorLineView.backgroundColor = UIColor.black
+        separatorLineView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(separatorLineView)
+        separatorLineView.leftAnchor.constraint(equalTo: containerView.leftAnchor).isActive = true
+        separatorLineView.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        separatorLineView.widthAnchor.constraint(equalTo: containerView.widthAnchor).isActive = true
+        separatorLineView.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
+    }
+    
+    @objc func handleSend(){
+        print(inputTextField.text)
+        inputTextField.resignFirstResponder()
+        inputTextField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        handleSend()
+        textField.resignFirstResponder()
+        textField.text = ""
+        return true
     }
 
     override func didReceiveMemoryWarning() {
